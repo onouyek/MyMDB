70c8811c6a4d6b33c7bdaae983f60a55c1988cc8
onouek <kohsuke.onoue@gmail.com>
2018-05-22 17:51:21 +0900

    Add user register view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.forms import (
+    UserCreationForm,
+)
+from django.urls import (
+    reverse_lazy,
+)
+from django.views.generic import (
+    CreateView,
+)
+
+class RegisterView(CreateView): # CreateViewを継承
+    template_name = 'user/register.html' # 次に作成する登録用のtemplate
+    form_class = UserCreationForm
+    success_url = reverse_lazy( # classベースのviewではreverse_lazy()を使う
+        'core:MovieList') # 登録に成功した場合のリダイレクト先
