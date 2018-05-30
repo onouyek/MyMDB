e3f12eaef1813f7ff5b3db0b2d6965237d6e1611
onouek <kohsuke.onoue@gmail.com>
2018-05-30 19:20:43 +0900

    Add Vote Model to core

--- core/models.py
+++ core/models.py
@@ -113,3 +113,30 @@ class Role(models.Model):
         unique_together = ('movie',
                            'person',
                            'name')
+
+
+class Vote(models.Model):
+    UP = 1
+    DOWN = -1
+    VALUE_CHOICES = (
+        (UP, "👍",),
+        (DOWN, "👎",),
+    )
+
+    value = models.SmallIntegerField(
+        choices=VALUE_CHOICES,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+    )
+    voted_on = models.DateTimeField(
+        auto_now=True
+    )
+
+    class Meta:
+        unique_together = ('user', 'movie')
