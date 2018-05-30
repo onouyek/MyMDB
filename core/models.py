0c236fafecaaba82395e738c85f7a5d9a512ed60
onouek <kohsuke.onoue@gmail.com>
2018-05-30 19:53:37 +0900

    Add VoteManager to check whether a user model has a related Vote instance for a given Movie instance

--- core/models.py
+++ core/models.py
@@ -116,6 +116,19 @@ class Role(models.Model):
                            'name')
 
 
+class VoteManager(models.Manager):
+
+    def get_vote_or_unsaved_blank_vote(self, movie, user):
+        try:
+            return Vote.objects.get(
+                movie=movie,
+                user=user)
+        except Vote.DoesNotExist:
+            return Vote(
+                movie=movie,
+                user=user)
+
+                
 class Vote(models.Model):
     UP = 1
     DOWN = -1
