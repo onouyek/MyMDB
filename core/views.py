d9cb4c86a9d3d2eed703f3235191c3d4bc750169
onouek <kohsuke.onoue@gmail.com>
2018-05-30 20:01:09 +0900

    Add MovieDetail get_context_data

--- core/views.py
+++ core/views.py
@@ -1,14 +1,48 @@
+from django.urls import reverse
 from django.views.generic import (
     ListView, DetailView,
 )
 
-from core.models import Movie, Person
+from core.forms import VoteForm
+from core.models import (
+    Movie,
+    Person,
+    Vote
+)
 
 
 class MovieDetail(DetailView):
     queryset = (
         Movie.objects
             .all_with_related_persons())
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            vote = Vote.objects.get_vote_or_unsaved_blank_vote(
+                movie=self.object,
+                user=self.request.user
+            )
+            if vote.id:
+                vote_form_url = reverse(
+                    'core:UpdateVote',
+                    kwargs={
+                        'movie_id': vote.movie.id,
+                        'pk': vote.id})
+            else:
+                vote_form_url = (
+                    reverse(
+                        'core:CreateVote',
+                        kwargs={
+                            'movie_id': self.object.id}
+                    )
+                )
+            vote_form = VoteForm(
+                instance=vote)
+            ctx['vote_form'] = vote_form
+            ctx['vote_form_url'] = vote_form_url
+
+        return ctx
 
 
 class MovieList(ListView):
