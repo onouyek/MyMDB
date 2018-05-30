702e36e0016254663d6b3f9638a2a230cbcf89af
onouek <kohsuke.onoue@gmail.com>
2018-05-30 20:19:56 +0900

    Add UpdateVote to views

--- core/views.py
+++ core/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.mixins import (
     LoginRequiredMixin)
-
-from django.views.generic import CreateView
+from django.core.exceptions import (
+    PermissionDenied)
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import (
-    ListView, DetailView,
+    ListView, DetailView, CreateView,
+    UpdateView
 )
 
 from core.forms import VoteForm
@@ -85,5 +86,30 @@ class PersonDetail(DetailView):
     queryset = Person.objects.all_with_prefetch_movies()
 
 
+class UpdateVote(LoginRequiredMixin, UpdateView):
+    form_class = VoteForm
+    queryset = Vote.objects.all()
 
+    def get_object(self, queryset=None):
+        vote = super().get_object(
+            queryset)
+        user = self.request.user
+        if vote.user != user:
+            raise PermissionDenied(
+                'cannot change another '
+                'users vote')
+        return vote
 
+    def get_success_url(self):
+        movie_id = self.object.movie.id
+        return reverse(
+            'core:MovieDetail',
+            kwargs={'pk': movie_id})
+
+    def render_to_response(self, context, **response_kwargs):
+        movie_id = context['object'].id
+        movie_detail_url = reverse(
+            'core:MovieDetail',
+            kwargs={'pk': movie_id})
+        return redirect(
+            to=movie_detail_url)
