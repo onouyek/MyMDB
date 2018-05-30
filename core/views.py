9a876402f08e0c39a6202a6aaafb27ab071a43e4
onouek <kohsuke.onoue@gmail.com>
2018-05-30 20:11:52 +0900

    Add CreateVote to views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin)
+
+from django.views.generic import CreateView
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import (
     ListView, DetailView,
@@ -9,6 +14,32 @@ from core.models import (
     Person,
     Vote
 )
+
+
+class CreateVote(LoginRequiredMixin, CreateView):
+    form_class = VoteForm
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['user'] = self.request.user.id
+        initial['movie'] = self.kwargs[
+            'movie_id']
+        return initial
+
+    def get_success_url(self):
+        movie_id = self.object.movie.id
+        return reverse(
+            'core:MovieDetail',
+            kwargs={
+                'pk': movie_id})
+
+    def render_to_response(self, context, **response_kwargs):
+        movie_id = context['object'].id
+        movie_detail_url = reverse(
+            'core:MovieDetail',
+            kwargs={'pk': movie_id})
+        return redirect(
+            to=movie_detail_url)
 
 
 class MovieDetail(DetailView):
