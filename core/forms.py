226ac7d2dc5d81511e28b4ca17959be166a37902
onouek <kohsuke.onoue@gmail.com>
2018-06-06 19:35:51 +0900

    Add MovieImage form

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from core.models import Vote, Movie
+from core.models import Vote, Movie, MovieImage
 
 
 class VoteForm(forms.ModelForm):
@@ -26,4 +26,26 @@ class VoteForm(forms.ModelForm):
         model = Vote
         fields = (
             'value', 'user', 'movie',
+        )
+
+
+class MovieImageForm(forms.ModelForm):
+
+    movie = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=Movie.objects.all(),
+        disabled=True,
+    )
+
+    user = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=get_user_model().
+        objects.all(),
+        disabled=True,
+    )
+
+    class Meta:
+        model = MovieImage
+        fields = (
+            'image', 'user', 'movie',
         )
