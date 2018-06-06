ae65b6c53222c17a20d36d31ada99475d472e876
onouek <kohsuke.onoue@gmail.com>
2018-06-06 19:53:25 +0900

    Add MovieImageUpload view

--- core/views.py
+++ core/views.py
@@ -81,8 +81,33 @@ class MovieDetail(DetailView):
         if self.request.user.is_authenticated:
             return MovieImageForm()
         return None
-        
 
+
+class MovieImageUpload(LoginRequiredMixin, CreateView):
+    form_class = MovieImageForm
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['user'] = self.request.user.id
+        initial['movie'] = self.kwargs['movie_id']
+        return initial
+
+    def render_to_response(self, context, **response_kwargs):
+        movie_id = self.kwargs['movie_id']
+        movie_detail_url = reverse(
+            'core:MovieDetail',
+            kwargs={'pk': movie_id})
+        return redirect(
+            to=movie_detail_url)
+
+    def get_success_url(self):
+        movie_id = self.kwargs['movie_id']
+        movie_detail_url = reverse(
+            'core:MovieDetail',
+            kwargs={'pk': movie_id})
+        return movie_detail_url
+
+        
 class MovieList(ListView):
     model = Movie
     paginate_by = 10
