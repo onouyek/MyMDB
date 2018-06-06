dc7e2f8a98e38ae68589d7b212911b37bcb79a17
onouek <kohsuke.onoue@gmail.com>
2018-06-06 19:27:03 +0900

    Add MovieImage model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.db import models
 from django.db.models.aggregates import (
     Sum
@@ -40,6 +42,23 @@ class Person(models.Model):
                 self.last_name,
                 self.first_name,
                 self.born)
+
+
+def movie_directory_path_with_uuid(
+    instance, filename):
+    return '{}/{}'.format(instance.movie_id, uuid4())
+
+
+class MovieImage(models.Model):
+    image = models.ImageField(
+        upload_to=movie_directory_path_with_uuid)
+    uploaded = models.DateTimeField(
+        auto_now_add=True)
+    movie = models.ForeignKey(
+        'Movie', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
 
 
 class MovieManager(models.Manager):
