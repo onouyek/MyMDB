03dd8cacb2df3626de575f10c293c79a644ff454
onouek <kohsuke.onoue@gmail.com>
2018-06-06 20:01:32 +0900

    Add MEIA_FILE_PATHS to root urls

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import (
+    static,)
 from django.urls import path, include
 from django.contrib import admin
 
 import core.urls
 import user.urls
+
+MEDIA_FILE_PATHS = static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +17,5 @@ urlpatterns = [
         user.urls, namespace='user')),
     path('', include(
         core.urls, namespace='core')),
-]
+] + MEDIA_FILE_PATHS
 
