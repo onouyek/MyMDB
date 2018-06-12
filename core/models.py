be2031a362ecaa8680cf05ac8ef6bb7a9261ded8
onouek <kohsuke.onoue@gmail.com>
2018-06-12 19:26:03 +0900

    Edit image upload filename

--- core/models.py
+++ core/models.py
@@ -46,7 +46,7 @@ class Person(models.Model):
 
 def movie_directory_path_with_uuid(
     instance, filename):
-    return '{}/{}'.format(instance.movie_id, uuid4())
+    return '{}/{}.{}'.format(instance.movie_id, uuid4(), filename.split('.'[-1]))
 
 
 class MovieImage(models.Model):
