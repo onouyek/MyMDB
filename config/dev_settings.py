6808b8ec3f69228ccdcb3b407dda2210c730a7c4
onouek <kohsuke.onoue@gmail.com>
2018-06-20 19:45:54 +0900

    Add debug_toolbar to dev settings

--- config/dev_settings.py
+++ config/dev_settings.py
@@ -3,3 +3,11 @@ from config.common_settings import *
 DEBUG = True
 
 SECRET_KEY = '_&@#veo_(o4nr^2cv2%*5isnk(ur!(o7*ttei^-#_wu4+(h#p1'
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
