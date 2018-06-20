1623a90283291766462784f0375c3a2cf56b1a4d
onouek <kohsuke.onoue@gmail.com>
2018-06-20 19:37:37 +0900

    Add STATIC_ROOT

--- config/common_settings.py
+++ config/common_settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'gathered_static_files')
 MEDIA_URL = '/uploaded/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '../media_root')
 
